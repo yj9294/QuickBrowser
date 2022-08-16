dcae631d8fe1939286f21917c60fa225f9c34d07
admin <497148400@qq.com>
2022-08-15 20:53:11 -0700

    Initial Commit

---
+++ QuickBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickBrowser
+//
+//  Created by admin on 8/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
